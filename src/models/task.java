80a4223512a7d669c3dbdbe2a18b5648617a5089
tty.smd <ttysmd0121@gmail.com>
2020-10-14 09:05:56 +0900

    Add database files

---
+++ src/models/task.java
@@ -0,0 +1,60 @@
+package models;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tasks")
+public class task {
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Timestamp getCreat_time() {
+        return creat_time;
+    }
+
+    public void setCreat_time(Timestamp creat_time) {
+        this.creat_time = creat_time;
+    }
+
+    public Timestamp getUpdate_time() {
+        return update_time;
+    }
+
+    public void setUpdate_time(Timestamp update_time) {
+        this.update_time = update_time;
+    }
+
+    public String getTask_content() {
+        return task_content;
+    }
+
+    public void setTask_content(String task_content) {
+        this.task_content = task_content;
+    }
+
+    @Column(name = "作成日時")
+    private Timestamp creat_time;
+
+    @Column(name = "更新日時")
+    private Timestamp update_time;
+
+    @Column(name = "タスク内容")
+    private String task_content;
+}
